14cc4a1478c320ab17c392b6978aa39d24a451de
dpat5941 <dpat59411@gmail.com>
2020-07-26 14:34:30 +1000

    Brexit question

---
+++ DP/Graph/brexit.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    C, P, X, L = map(int, input().split())
+    ls = []
+    for i in range(P):
+        temp1, temp2 = map(int, input().split())
+        ls.append((temp1,temp2))
